ae17c051ddb4fb8d210e4310ee8613d2fd8e20e4
Arin Kim <63277906+ArinKim@users.noreply.github.com>
2025-11-28 10:27:09 +0900

    init: initial project

---
+++ yumcoach/src/main/java/com/ssafy/yumcoach/YumcoachApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.yumcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumcoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumcoachApplication.class, args);
+	}
+
+}
---
+++ yumcoach/src/test/java/com/ssafy/yumcoach/YumcoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.yumcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumcoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
