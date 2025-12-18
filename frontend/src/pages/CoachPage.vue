ed6352393a0023866b277aca94b46326e3e851eb
ArinKim <63277906+ArinKim@users.noreply.github.com>
2025-12-19 00:26:31 +0900

    add: create CoachPage component with AppShell and StubPlaceholder

---
+++ frontend/src/pages/CoachPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <AppShell title="코치" subtitle="질문하면 바로 요약해줘요" @primary="noop">
+        <StubPlaceholder text="Coach (stub)" />
+    </AppShell>
+</template>
+
+<script setup>
+import AppShell from '@/layout/AppShell.vue'
+import StubPlaceholder from '@/components/base/StubPlaceholder.vue'
+function noop() { }
+</script>
