72604161b7674a893f79497ba30a710f2e5b1673
Dongni <dbehdgns1215@naver.com>
2025-12-21 01:46:28 +0900

    add: Pinia Plugin Persistedstate dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+// Pinia 퍼시스트 플러그인: 세션스토리지에 상태를 유지하도록 설정합니다.
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from "./App.vue";
 import router from "./router";
 import "./styles/tokens.css";
 
 const app = createApp(App);
-app.use(createPinia());
+const pinia = createPinia();
+// 세션 스토리지에 상태를 저장하도록 플러그인 등록
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia);
 app.use(router);
 app.mount("#app");
