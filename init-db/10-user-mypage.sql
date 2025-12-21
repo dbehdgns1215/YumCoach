0b639abd8c9d1d6b89c21252ae73705c98359815
Dongni <dbehdgns1215@naver.com>
2025-12-21 22:11:54 +0900

    add: user domain sql

---
+++ init-db/10-user-mypage.sql
@@ -0,0 +1,23 @@
+-- Add gender and age to user
+ALTER TABLE `user`
+ADD COLUMN `gender` VARCHAR(16) DEFAULT NULL,
+ADD COLUMN `age` INT DEFAULT NULL;
+
+-- Add nickname to user
+ALTER TABLE `user`
+ADD COLUMN `nickname` VARCHAR(64) DEFAULT NULL;
+
+-- Add activity_level to user_health
+ALTER TABLE `user_health`
+ADD COLUMN `activity_level` VARCHAR(32) DEFAULT 'SEDENTARY';
+
+-- Create user_diet_restriction table
+CREATE TABLE IF NOT EXISTS `user_diet_restriction` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `restriction_type` VARCHAR(64) NOT NULL,
+  `restriction_value` VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX (`user_id`),
+  CONSTRAINT `fk_udr_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
